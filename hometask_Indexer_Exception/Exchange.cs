a2504f5c68a9fdd4f0a1ab59fad7881263774c51
tebr1z <tabrizyh@code.edu.az>
2024-03-18 15:42:55 +0400

    Proje dosyası ekle.

---
+++ hometask_Indexer_Exception/Exchange.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hometask_Indexer_Exception
+{
+    public class Exchange
+    {
+        public double ConvertCurrency(Currency currency, double aznAmount)
+        {
+            double convertedAmount = 0;
+
+            switch (currency)
+            {
+                case Currency.Usd:
+                    convertedAmount = aznAmount * 0.59;
+                    break;
+                case Currency.Eur:
+                    convertedAmount = aznAmount * 0.49;
+                    break;
+                case Currency.Try:
+                    convertedAmount = aznAmount * 7.03;
+                    break;
+                default:
+                    Console.WriteLine("Yanlish valutya");
+                    break;
+            }
+
+            return convertedAmount;
+        }
+    }
+}
---
+++ hometask_Indexer_Exception/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace hometask_Indexer_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exchange exchange = new Exchange();
+
+            Console.WriteLine("AZN Deyerini daxil edin:");
+            double aznAmount = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Valyuta Sechin (Usd, Eur, Try):");
+            Currency currency;
+            if (!Enum.TryParse(Console.ReadLine(), true, out currency))
+            {
+                Console.WriteLine("Yalnish Valutya secimi ");
+                return;
+            }
+
+            double convertedAmount = exchange.ConvertCurrency(currency, aznAmount);
+            Console.WriteLine($"{aznAmount}  Manat  {currency}  Valutyasine cevrildi  {convertedAmount}  mebleg  ");
+        }
+    }
+}
